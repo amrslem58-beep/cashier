22b458c0fd33e93f27fe4fb9799d666f561b1b13
amrslem58-beep <amrslem58@gmail.com>
2026-09-20 16:26:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const C='pos-shell-v1',F=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(e.request.method!=='GET'||u.origin!==location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r})
+    .catch(()=>caches.match(e.request).then(r=>r||caches.match('index.html'))));
+});
